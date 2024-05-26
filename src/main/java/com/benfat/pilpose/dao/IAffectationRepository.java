ef8e617940e985868a82da3af5b5cd6011d7182d
Fatamri456@ <fatamri9@gmail.com>
2024-05-26 23:26:37 +0100

    add check imputation des heures

--- src/main/java/com/benfat/pilpose/dao/IAffectationRepository.java
+++ src/main/java/com/benfat/pilpose/dao/IAffectationRepository.java
@@ -23,6 +23,9 @@ public interface IAffectationRepository extends ICommonRepository<AffectationEnt
 	@Query("SELECT dce FROM AffectationEntity dce WHERE dce.idCollaborateur.idCollaborateur = :idc")
 	List<AffectationEntity> getByIdCollab(Long idc);
 	
+	@Query("SELECT dce FROM AffectationEntity dce WHERE dce.idCollaborateur.idCollaborateur = :idc and dce.idTache.typeTache ='tache'")
+	List<AffectationEntity> getByIdCollabTache(Long idc);
+	
 	@Query("SELECT DISTINCT dce.idCollaborateur FROM AffectationEntity dce WHERE dce.idTache = :idTache")
 	List<CollaborateurEntity> getByIdTache(TacheEntity idTache);
 	
